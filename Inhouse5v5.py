3d2b4401ba8166e57308411109dced49fe17f7ca
ChillinIX <Colin.j.prater@outlook.com>
2022-06-23 02:13:46 +1000

    updates

---
+++ Inhouse5v5.py
@@ -0,0 +1,15 @@
+import random
+
+pN = ('Chillin-Bot\playerNames.txt')
+
+with open(pN) as a1:
+    pLst = [line.rstrip('\n') for line in a1]
+a1.close()
+
+random.shuffle(pLst)
+radiant, dire = pLst[:5], pLst[5:]
+'''
+print("---------\n Radiant\n---------",*radiant, sep = "\n• ")
+print('\n')
+print("---------\n  Dire\n---------",*dire, sep = "\n• ")
+'''
--- __pycache__/main.py
+++ __pycache__/main.py
@@ -2,12 +2,13 @@ from turtle import color
 import discord
 from discord.ext import commands
 from Keys import *
-from Inhouse5v5 import *
 
 intents=discord.Intents.default()
 intents.members=True
 client=commands.Bot(command_prefix='!',intents=intents)
 channel = client.get_channel(987059865023287396)
+
+
 
 @client.event
 async def on_ready():
@@ -15,19 +16,15 @@ async def on_ready():
 
 @client.command()
 async def hello(ctx):
-  await ctx.send("Hi",ctx.author.display_name)
+  await ctx.send("Hi")
 
 @client.command()
-async def goodbot(ctx):
-  await ctx.send("Thanks, ",ctx.author.display_name)
+async def Goodbot(ctx):
+  await ctx.send("Thanks!")
 
 @client.command()
 async def split(ctx):
-  split = discord.Embed(title="5v5 Randomiser",color=0x03dffc)
-  split.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-  split.set_thumbnail(url="https://i.imgur.com/U2N9l0E.jpg")
-  split.add_field(name="Radiant",value=radiant, inline = True)
-  split.add_field(name="Dire",value=dire, inline = True)
+  split == discord.Embed(title="5v5 Randomiser", description="Good Luck!",color=0x03dffc)
   await ctx.send(embed=split)
 
 client.run(Token)
