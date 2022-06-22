6ab360d6edcb794111c8554a0e0da61f28803a0f
ChillinIX <Colin.j.prater@outlook.com>
2022-06-23 03:07:41 +1000

    updates

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from turtle import color
 import discord
+import numpy
+import random
 from discord.ext import commands
 from Keys import *
-from Inhouse5v5 import *
 
 intents=discord.Intents.default()
 intents.members=True
 client=commands.Bot(command_prefix='!',intents=intents)
 channel = client.get_channel(987059865023287396)
+
+players = ["Anthony","Shanise","Connor","Colin","Harry","Tim","Shaun","Ivan","Tristan","Ben"]
+random.shuffle(players)
+splitting = numpy.array_split(players,2)
 
 @client.event
 async def on_ready():
@@ -26,8 +30,8 @@ async def split(ctx):
   split = discord.Embed(title="5v5 Randomiser",color=0x03dffc)
   split.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
   split.set_thumbnail(url="https://i.imgur.com/U2N9l0E.jpg")
-  split.add_field(name="Radiant",value=radiant, inline = True)
-  split.add_field(name="Dire",value=dire, inline = True)
+  split.add_field(name="Radiant",value=splitting[0], inline = True)
+  split.add_field(name="Dire",value=splitting[1], inline = True)
   await ctx.send(embed=split)
 
 client.run(Token)
