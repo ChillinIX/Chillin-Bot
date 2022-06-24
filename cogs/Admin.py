151d2c0f37264f1daa0fa1457923370d125eb983
ChillinIX <Colin.j.prater@outlook.com>
2022-06-24 16:23:20 +1000

    utilizing cogs

---
+++ cogs/Admin.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+import os
+
+class Admin(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+    
+    #Commands
+
+def setup(client):
+  client.add_cog(Admin(client))
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
-import numpy
-import random
 from discord.ext import commands
+import os
 from Keys import *
 
 intents=discord.Intents.default()
@@ -9,29 +8,19 @@ intents.members=True
 client=commands.Bot(command_prefix='!',intents=intents)
 channel = client.get_channel(987059865023287396)
 
-players = ["Anthony","Shanise","Connor","Colin","Harry","Tim","Shaun","Ivan","Tristan","Ben"]
-random.shuffle(players)
-splitting = numpy.array_split(players,2)
-
 @client.event
 async def on_ready():
   print('Logged in as {0.user}'.format(client))
 
-@client.command()
-async def hello(ctx):
-  await ctx.send("Hi",ctx.author.display_name)
+inital_extensions = []
 
-@client.command()
-async def goodbot(ctx):
-  await ctx.send("Thanks, ",ctx.author.display_name)
+for filename in os.listdir('Chillin-Bot\cogs'):
+  if filename.endswith('.py'):
+    inital_extensions.append("cogs." +filename[:-3])
 
-@client.command()
-async def split(ctx):
-  split = discord.Embed(title="5v5 Randomiser",color=0x03dffc)
-  split.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-  split.set_thumbnail(url="https://i.imgur.com/U2N9l0E.jpg")
-  split.add_field(name="Radiant",value=splitting[0], inline = True)
-  split.add_field(name="Dire",value=splitting[1], inline = True)
-  await ctx.send(embed=split)
+if __name__=='__main__':
+  for extensions in inital_extensions:
+    client.load_extension(extensions)
+
 
 client.run(Token)
