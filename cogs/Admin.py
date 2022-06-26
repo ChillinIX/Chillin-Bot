a480d44d7077504fd7691059bbf4114e1aed706a
ChillinIX <Colin.j.prater@outlook.com>
2022-06-26 18:29:46 +1000

    updates

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -5,8 +5,6 @@ import os
 class Admin(commands.Cog):
     def __init__(self,client):
         self.client = client
-    
-    #Commands
-    #additions
+
 def setup(client):
   client.add_cog(Admin(client))
