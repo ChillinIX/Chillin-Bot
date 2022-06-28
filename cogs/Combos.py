480b4bbe9358e2c60e52cb28047225f2ecbf7106
ChillinIX <>
2022-06-28 14:08:40 +1000

    updates

---
+++ cogs/Combos.py
@@ -0,0 +1,37 @@
+'''
+Hold & Fold
+1. Slark
+2. Puck
+3. Centaur Warrunner
+4. Undying
+5. Warlock/Lich
+
+Stunner Deal
+1. Sven / Wraith King
+2. Queen of Pain
+3. Riki
+4. Dawnbreaker
+5. Grimstroke
+
+Wheelhouse beta
+1. Sniper
+2. Zeus
+3. Centaur Warrunner
+4. Bounty Hunter
+5. Vengeful Spirit
+
+Wheelhouse
+1. Phantom Assassin
+2. Outworld Devourer
+3. Spirit Breaker
+4. Tide Hunter
+5. Oracle
+
+Rat Master
+1. Natures Prophet
+2. Pangolier
+3. Underlord
+4. Tidehunter
+5. Undying
+
+'''
