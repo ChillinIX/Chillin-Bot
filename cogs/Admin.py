4039a8b4b277729b6391867bc24fe493eb23169a
ChillinIX <Colin.j.prater@outlook.com>
2022-07-27 09:16:58 +1000

    adding the !amideveloper command

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -1,10 +1,15 @@
 import discord
 from discord.ext import commands
-import os
 
 class admin(commands.Cog):
-    def __init__(self,client):
-        self.client = client
+  def __init__(self,client):
+    self.client = client
+
+  @commands.command()
+  async def amideveloper(self,ctx):
+    if ctx.author.id == 198622235236237312:
+      return await ctx.send(f"Of course you are {ctx.author.name}... you wrote me")
+    await ctx.send(f"no, heck off {ctx.author.name}")
 
 def setup(client):
   client.add_cog(admin(client))
