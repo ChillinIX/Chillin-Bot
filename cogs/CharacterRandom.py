dea6200da633024b21a60732dd5cc009d3684f0f
ChillinIX <Colin.j.prater@outlook.com>
2022-07-27 10:31:19 +1000

    adding team name randomiser

--- cogs/CharacterRandom.py
+++ cogs/CharacterRandom.py
@@ -1,42 +1,29 @@
 import discord
-import random
 from discord.ext import commands
+import random
 
 class characterRandom(commands.Cog):
   def __init__(self,client):
     self.client = client
 
-
   @commands.command()
   async def giveteam(self,ctx):
     
-    # Declare Variables
-    filename1 = open('cogs\DB\f1.txt','r')
-    filename2 = open('cogs\DB\f2.txt','r')
-
-    # Open each .txt file using a loop into a list(s) named wordList#. Strip the new lines from each word.
-    with open(filename1) as f1:
-      wordList1 = [line.rstrip('\n') for line in f1]
-
-    with open(filename2) as f2:
-      wordList2 = [line.rstrip('\n') for line in f2]
+    filename1=open('cogs/DB/f1.txt','r')
+    wordList1=[line.rstrip('\n') for line in filename1]
+    filename1.close()
+    out1 = random.choice(wordList1)
     
-    # Close the .txt files.
-    f1.close()
-    f2.close()
+    filename2=open('cogs/DB/f2.txt','r')
+    wordList2=[line.rstrip('\n') for line in filename2]
+    filename2.close()
+    out2 = random.choice(wordList2)
     
-    # Randomly select a index value in each list using the choice module within Random.
-    # Concatenate the string using a simple print statement. 
-    print(random.choice(wordList1) + "_" + random.choice(wordList2))
+    return await ctx.send(f"Team Name: {out1} {out2}")
 
 
 
 # random player, random character
-
-#tie them together like the animal adjective program
-#name the teamlike so:
-
-#Team Awkward Aardvark!
 
 #Tim - Axe
 #Shanise - Underlord
